5d4381a5b1d1da227d65ca9a61797c19759ab5c1
Vladislav <bombardire54@gmail.com>
2021-11-10 15:32:51 +0200

    Store object was added to project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import reportWebVitals from './reportWebVitals'
-import { state, observer } from './redux/state'
+import { store } from './redux/state'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
-import { addPost } from './redux/state'
 
 const rerenderTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} addPost={addPost} />
+        <App state={store.getState()} addPost={store.addPost.bind(store)} />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-observer(rerenderTree)
-rerenderTree(state)
+store.subscribe(rerenderTree)
+rerenderTree(store.getState())
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,46 +1,54 @@
-let rerender = undefined
+export const store = {
+    _state: {
+        profilePage: {
+            photo: null,
+            theme: null,
+            status: null,
+            currentPostText: '',
+            posts: [
+                {id: 1, text: 'Hello, how are doing?'},
+                {id: 2, text: 'React native? whaaat?'}
+            ]
+        },
+        usersPage: {
+            users: [
+                {id: 1, name: 'Viktor', status: 'happy', work: 'React developer', image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
+                {id: 2, name: 'Nikita', status: 'happy', work: 'React developer' , image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
+                {id: 3, name: 'Vlad', status: 'happy', work: 'React developer', image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
+                {id: 4, name: 'Peter', status: 'happy', work: 'React developer', image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
+            ]
+        },
+        dialogsPage: {
+            users: [
+                {id: 1, name: 'Vasya', messages: [
+                                            {id: 1, text: 'hello', sender: 0},
+                                            {id: 2, text: 'hi', sender: 1},
+                                        ]
+                },
+                {id: 2, name: 'Gleb', messages: [
+                                            {id: 1, text: 'hello', sender: 0},
+                                            {id: 2, text: 'hi', sender: 1},
+                                        ]
+                }
+            ]
+        }
+    },
 
-export const state = {
-    profilePage: {
-        photo: null,
-        theme: null,
-        status: null,
-        currentPostText: '',
-        posts: [
-            {id: 1, text: 'Hello, how are doing?'},
-            {id: 2, text: 'React native? whaaat?'}
-        ]
+    _rerender: function() {},
+    
+    getState() {
+        return this._state
     },
-    usersPage: {
-        users: [
-            {id: 1, name: 'Viktor', status: 'happy', work: 'React developer', image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
-            {id: 2, name: 'Nikita', status: 'happy', work: 'React developer' , image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
-            {id: 3, name: 'Vlad', status: 'happy', work: 'React developer', image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
-            {id: 4, name: 'Peter', status: 'happy', work: 'React developer', image: 'https://miro.medium.com/max/684/1*wHDylzqUD_9HmbDUOiDg1Q.png'},
-        ]
+
+
+    addPost(text) {
+        console.log(this)
+        const newPost = {id: 3, text: text}
+        this._state.profilePage.posts.unshift(newPost)
+        this._rerender(this._state)
     },
-    dialogsPage: {
-        users: [
-            {id: 1, name: 'Vasya', messages: [
-                                        {id: 1, text: 'hello', sender: 0},
-                                        {id: 2, text: 'hi', sender: 1},
-                                    ]
-            },
-            {id: 2, name: 'Gleb', messages: [
-                                        {id: 1, text: 'hello', sender: 0},
-                                        {id: 2, text: 'hi', sender: 1},
-                                    ]
-            }
-        ]
+    
+    subscribe(observer) {
+        this._rerender = observer
     }
-}
-
-export const addPost = (text) => {
-    const newPost = {id: 3, text: text}
-    state.profilePage.posts.unshift(newPost)
-    rerender(state)
-}
-
-export const observer = (func) => {
-    rerender = func
 }
